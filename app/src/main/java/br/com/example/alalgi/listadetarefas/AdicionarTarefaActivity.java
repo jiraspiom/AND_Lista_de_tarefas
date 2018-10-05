ce81d31788a42dca5065ae94854e72125f3dc313
gilmar <jiraspiom@yahoo.com.br>
2018-10-05 14:41:28 -0300

    Adicionado activite Adicionar tarefa

---
+++ app/src/main/java/br/com/example/alalgi/listadetarefas/AdicionarTarefaActivity.java
@@ -0,0 +1,13 @@
+package br.com.example.alalgi.listadetarefas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
